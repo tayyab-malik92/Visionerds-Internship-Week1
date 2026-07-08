39242d5bb247483dab0c8ea5f36f92bc1934da38
Muhammad Tayyab Malik <muhammadtayyabmalik92@gmail.com>
2026-07-08 16:02:37 +0500

    Completed Visionerds Day 3 - Interactive Chatbot

---
+++ config.py
@@ -0,0 +1,9 @@
+SYSTEM_PROMPT = """
+You are a friendly AI assistant.
+
+Rules:
+- Answer clearly.
+- Be polite.
+- Explain step by step.
+- Help beginners learn programming and AI.
+"""
---
+++ day3.py
@@ -0,0 +1,41 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+from config import SYSTEM_PROMPT
+
+# Load variables from the .env file
+load_dotenv()
+
+# Create the client
+client = OpenAI(
+    api_key=os.getenv("GROQ_API_KEY"),
+    base_url="https://api.groq.com/openai/v1"
+)
+
+print("🤖 AI Chatbot Started made by TAYYAB!")
+print("Type 'exit' to quit.\n")
+
+while True:
+    user_input = input("You: ")
+    if not user_input.strip():
+     continue
+    if user_input.lower() == "exit":
+        print("Goodbye! 👋")
+        break
+
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        temperature=0.7,
+        messages=[
+            {
+                "role": "system",
+                "content": SYSTEM_PROMPT
+            },
+            {
+                "role": "user",
+                "content": user_input
+            }
+        ]
+    )
+
+    print("AI:", response.choices[0].message.content)
